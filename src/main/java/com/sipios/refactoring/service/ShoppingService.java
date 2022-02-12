796e68a8ab07c33b73cee351987c8aad2094eddc
mabdelhedi <mabdelhedi@ippon.fr>
2022-02-12 10:58:24 +0100

    Move shopping logic in service

---
+++ src/main/java/com/sipios/refactoring/service/ShoppingService.java
@@ -0,0 +1,9 @@
+package com.sipios.refactoring.service;
+
+import com.sipios.refactoring.model.Body;
+
+public interface ShoppingService {
+
+    double calculateItemsPrice(final Body body);
+
+}
---
+++ src/main/java/com/sipios/refactoring/service/impl/ShoppingServiceImpl.java
@@ -0,0 +1,115 @@
+package com.sipios.refactoring.service.impl;
+
+import com.sipios.refactoring.model.Body;
+import com.sipios.refactoring.model.Item;
+import com.sipios.refactoring.service.ShoppingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+@Service
+public class ShoppingServiceImpl implements ShoppingService {
+
+
+    @Override
+    public double calculateItemsPrice(Body body) {
+        double p = 0;
+        double d;
+
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
+        cal.setTime(date);
+
+        // Compute discount for customer
+        if (body.getType().equals("STANDARD_CUSTOMER")) {
+            d = 1;
+        } else if (body.getType().equals("PREMIUM_CUSTOMER")) {
+            d = 0.9;
+        } else if (body.getType().equals("PLATINUM_CUSTOMER")) {
+            d = 0.5;
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        // Compute total amount depending on the types and quantity of product and
+        // if we are in winter or summer discounts periods
+        if (
+            !(
+                cal.get(Calendar.DAY_OF_MONTH) < 15 &&
+                    cal.get(Calendar.DAY_OF_MONTH) > 5 &&
+                    cal.get(Calendar.MONTH) == 5
+            ) &&
+                !(
+                    cal.get(Calendar.DAY_OF_MONTH) < 15 &&
+                        cal.get(Calendar.DAY_OF_MONTH) > 5 &&
+                        cal.get(Calendar.MONTH) == 0
+                )
+        ) {
+            if (body.getItems() == null) {
+                return 0;
+            }
+
+            for (int i = 0; i < body.getItems().length; i++) {
+                Item it = body.getItems()[i];
+
+                if (it.getType().equals("TSHIRT")) {
+                    p += 30 * it.getNb() * d;
+                } else if (it.getType().equals("DRESS")) {
+                    p += 50 * it.getNb() * d;
+                } else if (it.getType().equals("JACKET")) {
+                    p += 100 * it.getNb() * d;
+                }
+                // else if (it.getType().equals("SWEATSHIRT")) {
+                //     price += 80 * it.getNb();
+                // }
+            }
+        } else {
+            if (body.getItems() == null) {
+                return 0;
+            }
+
+            for (int i = 0; i < body.getItems().length; i++) {
+                Item it = body.getItems()[i];
+
+                if (it.getType().equals("TSHIRT")) {
+                    p += 30 * it.getNb() * d;
+                } else if (it.getType().equals("DRESS")) {
+                    p += 50 * it.getNb() * 0.8 * d;
+                } else if (it.getType().equals("JACKET")) {
+                    p += 100 * it.getNb() * 0.9 * d;
+                }
+                // else if (it.getType().equals("SWEATSHIRT")) {
+                //     price += 80 * it.getNb();
+                // }
+            }
+        }
+
+        try {
+            if (body.getType().equals("STANDARD_CUSTOMER")) {
+                if (p > 200) {
+                    throw new Exception("Price (" + p + ") is too high for standard customer");
+                }
+            } else if (body.getType().equals("PREMIUM_CUSTOMER")) {
+                if (p > 800) {
+                    throw new Exception("Price (" + p + ") is too high for premium customer");
+                }
+            } else if (body.getType().equals("PLATINUM_CUSTOMER")) {
+                if (p > 2000) {
+                    throw new Exception("Price (" + p + ") is too high for platinum customer");
+                }
+            } else {
+                if (p > 200) {
+                    throw new Exception("Price (" + p + ") is too high for standard customer");
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+
+        return d;
+    }
+}
